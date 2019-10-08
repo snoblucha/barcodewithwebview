903c9af822d61af2fe0bb890a627a00aa3f1a678
Petr Šnobl <petr.snobl@goldengate.cz>
2019-10-08 13:33:23 +0200

    Zobrazit ve webview

--- app/src/main/java/com/varvet/barcodereadersample/MainActivity.kt
+++ app/src/main/java/com/varvet/barcodereadersample/MainActivity.kt
@@ -1,28 +1,52 @@
 package com.varvet.barcodereadersample
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.vision.barcode.Barcode
 import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mResultTextView: TextView
+    private lateinit var mResultWebView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mResultTextView = findViewById(R.id.result_textview)
+        mResultWebView = findViewById(R.id.result_webview)
+        mResultWebView.webViewClient = WebViewClient()
+        mResultWebView.settings.builtInZoomControls = true
+
 
         findViewById<Button>(R.id.scan_barcode_button).setOnClickListener {
             val intent = Intent(applicationContext, BarcodeCaptureActivity::class.java)
             startActivityForResult(intent, BARCODE_READER_REQUEST_CODE)
+        }
+        val rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+        if (rc != PackageManager.PERMISSION_GRANTED) {
+            requestInternetPermission()
+        }
+    }
+
+    private fun requestInternetPermission() {
+        Log.w(LOG_TAG, "Camera permission is not granted. Requesting permission")
+
+        val permissions = arrayOf(Manifest.permission.INTERNET)
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.INTERNET)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_INTERNET_PERM)
         }
     }
 
@@ -32,9 +56,10 @@ class MainActivity : AppCompatActivity() {
                 if (data != null) {
                     val barcode = data.getParcelableExtra<Barcode>(BarcodeCaptureActivity.BarcodeObject)
                     val p = barcode.cornerPoints
-                    mResultTextView.text = barcode.displayValue
+                    mResultWebView.loadUrl(barcode.displayValue)
+
                 } else
-                    mResultTextView.setText(R.string.no_barcode_captured)
+                    mResultWebView.loadUrl("about:blank")
             } else
                 Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
                         CommonStatusCodes.getStatusCodeString(resultCode)))
@@ -42,8 +67,55 @@ class MainActivity : AppCompatActivity() {
             super.onActivityResult(requestCode, resultCode, data)
     }
 
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on [.requestPermissions].
+     *
+     *
+     * **Note:** It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     *
+     *
+     * @param requestCode  The request code passed in [.requestPermissions].
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     * which is either [PackageManager.PERMISSION_GRANTED]
+     * or [PackageManager.PERMISSION_DENIED]. Never null.
+     * @see .requestPermissions
+     */
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode != RC_HANDLE_INTERNET_PERM) {
+            Log.d(LOG_TAG, "Got unexpected permission result: $requestCode")
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults.size != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(LOG_TAG, "Internet permission granted - initialize the camera source")
+            return
+        }
+
+        Log.e(LOG_TAG, "Permission not granted: results len = " + grantResults.size +
+                " Result code = " + if (grantResults.size > 0) grantResults[0] else "(empty)")
+
+        val listener = DialogInterface.OnClickListener { dialog, id -> finish() }
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Multitracker sample")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show()
+    }
+
+
     companion object {
         private val LOG_TAG = MainActivity::class.java.simpleName
         private val BARCODE_READER_REQUEST_CODE = 1
+        // Permission request codes need to be < 256
+        private val RC_HANDLE_INTERNET_PERM = 2
+
     }
 }
